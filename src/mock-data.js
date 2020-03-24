5c770474d4a01426d415287b62c00a5f95ea289b
tyson-rodriguez25 <43011932+tyson-rodriguez25@users.noreply.github.com>
2020-03-23 19:49:17 -0600

    Create mock-data.js

---
+++ src/mock-data.js
@@ -0,0 +1,168 @@
+let mock = [{
+  role: [{
+    attackers: [{
+      id:1,
+      operator: "Mute",
+      name: "Mark R. Chandar",
+      image: '',
+      gadget: "SIGNAL DISRUPTOR",
+      Bio:"“People think my codename is because I don’t like to talk. Maybe I just don’t like you.”An exceptional and well-rounded student, Chandar was one of the youngest accepted into the University of Cambridge. He majored in electronics and computer engineering, completing an internship at a prestigious tech company where he contributed to the design and function of security system prototypes. His ingenuity caught the attention of Britain’s national security office, the Government Communications Headquarters (GCHQ). Having demonstrated acuity in code breaking, Chandar became an intelligence officer within the signals intelligence (SIGINT) unit. He continues to contribute to the applied technical research team and has recently expanded his field of study to include learning Korean."
+      },
+      {
+        id:2,
+        operator: "Smoke",
+        name: "Mark R. Chandar",
+        image: '',
+        gadget: "SIGNAL DISRUPTOR",
+        Bio:"“People think my codename is because I don’t like to talk. Maybe I just don’t like you.”An exceptional and well-rounded student, Chandar was one of the youngest accepted into the University of Cambridge. He majored in electronics and computer engineering, completing an internship at a prestigious tech company where he contributed to the design and function of security system prototypes. His ingenuity caught the attention of Britain’s national security office, the Government Communications Headquarters (GCHQ). Having demonstrated acuity in code breaking, Chandar became an intelligence officer within the signals intelligence (SIGINT) unit. He continues to contribute to the applied technical research team and has recently expanded his field of study to include learning Korean."
+      },
+      {
+        id:3,
+        operator: "Sledge",
+        name: "Mark R. Chandar",
+        image: '',
+        gadget: "SIGNAL DISRUPTOR",
+        Bio:"“People think my codename is because I don’t like to talk. Maybe I just don’t like you.”An exceptional and well-rounded student, Chandar was one of the youngest accepted into the University of Cambridge. He majored in electronics and computer engineering, completing an internship at a prestigious tech company where he contributed to the design and function of security system prototypes. His ingenuity caught the attention of Britain’s national security office, the Government Communications Headquarters (GCHQ). Having demonstrated acuity in code breaking, Chandar became an intelligence officer within the signals intelligence (SIGINT) unit. He continues to contribute to the applied technical research team and has recently expanded his field of study to include learning Korean."
+      },
+      {
+        id:4,
+        operator: "Thatcher",
+        name: "Mark R. Chandar",
+        image: '',
+        gadget: "SIGNAL DISRUPTOR",
+        Bio:"“People think my codename is because I don’t like to talk. Maybe I just don’t like you.”An exceptional and well-rounded student, Chandar was one of the youngest accepted into the University of Cambridge. He majored in electronics and computer engineering, completing an internship at a prestigious tech company where he contributed to the design and function of security system prototypes. His ingenuity caught the attention of Britain’s national security office, the Government Communications Headquarters (GCHQ). Having demonstrated acuity in code breaking, Chandar became an intelligence officer within the signals intelligence (SIGINT) unit. He continues to contribute to the applied technical research team and has recently expanded his field of study to include learning Korean."
+      },
+      {
+        id:5,
+        operator: "Mute",
+        name: "Mark R. Chandar",
+        image: '',
+        gadget: "SIGNAL DISRUPTOR",
+        Bio:"“People think my codename is because I don’t like to talk. Maybe I just don’t like you.”An exceptional and well-rounded student, Chandar was one of the youngest accepted into the University of Cambridge. He majored in electronics and computer engineering, completing an internship at a prestigious tech company where he contributed to the design and function of security system prototypes. His ingenuity caught the attention of Britain’s national security office, the Government Communications Headquarters (GCHQ). Having demonstrated acuity in code breaking, Chandar became an intelligence officer within the signals intelligence (SIGINT) unit. He continues to contribute to the applied technical research team and has recently expanded his field of study to include learning Korean."
+      },
+      {
+        id:6,
+        operator: "Mute",
+        name: "Mark R. Chandar",
+        image: '',
+        gadget: "SIGNAL DISRUPTOR",
+        Bio:"“People think my codename is because I don’t like to talk. Maybe I just don’t like you.”An exceptional and well-rounded student, Chandar was one of the youngest accepted into the University of Cambridge. He majored in electronics and computer engineering, completing an internship at a prestigious tech company where he contributed to the design and function of security system prototypes. His ingenuity caught the attention of Britain’s national security office, the Government Communications Headquarters (GCHQ). Having demonstrated acuity in code breaking, Chandar became an intelligence officer within the signals intelligence (SIGINT) unit. He continues to contribute to the applied technical research team and has recently expanded his field of study to include learning Korean."
+      },
+      {
+        id:7,
+        operator: "Mute",
+        name: "Mark R. Chandar",
+        image: '',
+        gadget: "SIGNAL DISRUPTOR",
+        Bio:"“People think my codename is because I don’t like to talk. Maybe I just don’t like you.”An exceptional and well-rounded student, Chandar was one of the youngest accepted into the University of Cambridge. He majored in electronics and computer engineering, completing an internship at a prestigious tech company where he contributed to the design and function of security system prototypes. His ingenuity caught the attention of Britain’s national security office, the Government Communications Headquarters (GCHQ). Having demonstrated acuity in code breaking, Chandar became an intelligence officer within the signals intelligence (SIGINT) unit. He continues to contribute to the applied technical research team and has recently expanded his field of study to include learning Korean."
+      },
+      {
+        id:8,
+        operator: "Mute",
+        name: "Mark R. Chandar",
+        image: '',
+        gadget: "SIGNAL DISRUPTOR",
+        Bio:"“People think my codename is because I don’t like to talk. Maybe I just don’t like you.”An exceptional and well-rounded student, Chandar was one of the youngest accepted into the University of Cambridge. He majored in electronics and computer engineering, completing an internship at a prestigious tech company where he contributed to the design and function of security system prototypes. His ingenuity caught the attention of Britain’s national security office, the Government Communications Headquarters (GCHQ). Having demonstrated acuity in code breaking, Chandar became an intelligence officer within the signals intelligence (SIGINT) unit. He continues to contribute to the applied technical research team and has recently expanded his field of study to include learning Korean."
+      },
+      {
+        id:9,
+        operator: "Mute",
+        name: "Mark R. Chandar",
+        image: '',
+        gadget: "SIGNAL DISRUPTOR",
+        Bio:"“People think my codename is because I don’t like to talk. Maybe I just don’t like you.”An exceptional and well-rounded student, Chandar was one of the youngest accepted into the University of Cambridge. He majored in electronics and computer engineering, completing an internship at a prestigious tech company where he contributed to the design and function of security system prototypes. His ingenuity caught the attention of Britain’s national security office, the Government Communications Headquarters (GCHQ). Having demonstrated acuity in code breaking, Chandar became an intelligence officer within the signals intelligence (SIGINT) unit. He continues to contribute to the applied technical research team and has recently expanded his field of study to include learning Korean."
+      },
+      {
+        id:10,
+        operator: "Mute",
+        name: "Mark R. Chandar",
+        image: '',
+        gadget: "SIGNAL DISRUPTOR",
+        Bio:"“People think my codename is because I don’t like to talk. Maybe I just don’t like you.”An exceptional and well-rounded student, Chandar was one of the youngest accepted into the University of Cambridge. He majored in electronics and computer engineering, completing an internship at a prestigious tech company where he contributed to the design and function of security system prototypes. His ingenuity caught the attention of Britain’s national security office, the Government Communications Headquarters (GCHQ). Having demonstrated acuity in code breaking, Chandar became an intelligence officer within the signals intelligence (SIGINT) unit. He continues to contribute to the applied technical research team and has recently expanded his field of study to include learning Korean."
+      }
+  ],
+    defenders: [{
+      id:11,
+      operator: "Mute",
+      name: "Mark R. Chandar",
+      image: '',
+      gadget: "SIGNAL DISRUPTOR",
+      Bio:"“People think my codename is because I don’t like to talk. Maybe I just don’t like you.”An exceptional and well-rounded student, Chandar was one of the youngest accepted into the University of Cambridge. He majored in electronics and computer engineering, completing an internship at a prestigious tech company where he contributed to the design and function of security system prototypes. His ingenuity caught the attention of Britain’s national security office, the Government Communications Headquarters (GCHQ). Having demonstrated acuity in code breaking, Chandar became an intelligence officer within the signals intelligence (SIGINT) unit. He continues to contribute to the applied technical research team and has recently expanded his field of study to include learning Korean."
+      },
+      {
+        id:12,
+        operator: "Smoke",
+        name: "Mark R. Chandar",
+        image: '',
+        gadget: "SIGNAL DISRUPTOR",
+        Bio:"“People think my codename is because I don’t like to talk. Maybe I just don’t like you.”An exceptional and well-rounded student, Chandar was one of the youngest accepted into the University of Cambridge. He majored in electronics and computer engineering, completing an internship at a prestigious tech company where he contributed to the design and function of security system prototypes. His ingenuity caught the attention of Britain’s national security office, the Government Communications Headquarters (GCHQ). Having demonstrated acuity in code breaking, Chandar became an intelligence officer within the signals intelligence (SIGINT) unit. He continues to contribute to the applied technical research team and has recently expanded his field of study to include learning Korean."
+      },
+      {
+        id:13,
+        operator: "Sledge",
+        name: "Mark R. Chandar",
+        image: '',
+        gadget: "SIGNAL DISRUPTOR",
+        Bio:"“People think my codename is because I don’t like to talk. Maybe I just don’t like you.”An exceptional and well-rounded student, Chandar was one of the youngest accepted into the University of Cambridge. He majored in electronics and computer engineering, completing an internship at a prestigious tech company where he contributed to the design and function of security system prototypes. His ingenuity caught the attention of Britain’s national security office, the Government Communications Headquarters (GCHQ). Having demonstrated acuity in code breaking, Chandar became an intelligence officer within the signals intelligence (SIGINT) unit. He continues to contribute to the applied technical research team and has recently expanded his field of study to include learning Korean."
+      },
+      {
+        id:14,
+        operator: "Thatcher",
+        name: "Mark R. Chandar",
+        image: '',
+        gadget: "SIGNAL DISRUPTOR",
+        Bio:"“People think my codename is because I don’t like to talk. Maybe I just don’t like you.”An exceptional and well-rounded student, Chandar was one of the youngest accepted into the University of Cambridge. He majored in electronics and computer engineering, completing an internship at a prestigious tech company where he contributed to the design and function of security system prototypes. His ingenuity caught the attention of Britain’s national security office, the Government Communications Headquarters (GCHQ). Having demonstrated acuity in code breaking, Chandar became an intelligence officer within the signals intelligence (SIGINT) unit. He continues to contribute to the applied technical research team and has recently expanded his field of study to include learning Korean."
+      },
+      {
+        id:15,
+        operator: "Mute",
+        name: "Mark R. Chandar",
+        image: '',
+        gadget: "SIGNAL DISRUPTOR",
+        Bio:"“People think my codename is because I don’t like to talk. Maybe I just don’t like you.”An exceptional and well-rounded student, Chandar was one of the youngest accepted into the University of Cambridge. He majored in electronics and computer engineering, completing an internship at a prestigious tech company where he contributed to the design and function of security system prototypes. His ingenuity caught the attention of Britain’s national security office, the Government Communications Headquarters (GCHQ). Having demonstrated acuity in code breaking, Chandar became an intelligence officer within the signals intelligence (SIGINT) unit. He continues to contribute to the applied technical research team and has recently expanded his field of study to include learning Korean."
+      },
+      {
+        id:16,
+        operator: "Mute",
+        name: "Mark R. Chandar",
+        image: '',
+        gadget: "SIGNAL DISRUPTOR",
+        Bio:"“People think my codename is because I don’t like to talk. Maybe I just don’t like you.”An exceptional and well-rounded student, Chandar was one of the youngest accepted into the University of Cambridge. He majored in electronics and computer engineering, completing an internship at a prestigious tech company where he contributed to the design and function of security system prototypes. His ingenuity caught the attention of Britain’s national security office, the Government Communications Headquarters (GCHQ). Having demonstrated acuity in code breaking, Chandar became an intelligence officer within the signals intelligence (SIGINT) unit. He continues to contribute to the applied technical research team and has recently expanded his field of study to include learning Korean."
+      },
+      {
+        id:17,
+        operator: "Mute",
+        name: "Mark R. Chandar",
+        image: '',
+        gadget: "SIGNAL DISRUPTOR",
+        Bio:"“People think my codename is because I don’t like to talk. Maybe I just don’t like you.”An exceptional and well-rounded student, Chandar was one of the youngest accepted into the University of Cambridge. He majored in electronics and computer engineering, completing an internship at a prestigious tech company where he contributed to the design and function of security system prototypes. His ingenuity caught the attention of Britain’s national security office, the Government Communications Headquarters (GCHQ). Having demonstrated acuity in code breaking, Chandar became an intelligence officer within the signals intelligence (SIGINT) unit. He continues to contribute to the applied technical research team and has recently expanded his field of study to include learning Korean."
+      },
+      {
+        id:18,
+        operator: "Mute",
+        name: "Mark R. Chandar",
+        image: '',
+        gadget: "SIGNAL DISRUPTOR",
+        Bio:"“People think my codename is because I don’t like to talk. Maybe I just don’t like you.”An exceptional and well-rounded student, Chandar was one of the youngest accepted into the University of Cambridge. He majored in electronics and computer engineering, completing an internship at a prestigious tech company where he contributed to the design and function of security system prototypes. His ingenuity caught the attention of Britain’s national security office, the Government Communications Headquarters (GCHQ). Having demonstrated acuity in code breaking, Chandar became an intelligence officer within the signals intelligence (SIGINT) unit. He continues to contribute to the applied technical research team and has recently expanded his field of study to include learning Korean."
+      },
+      {
+        id:19,
+        operator: "Mute",
+        name: "Mark R. Chandar",
+        image: '',
+        gadget: "SIGNAL DISRUPTOR",
+        Bio:"“People think my codename is because I don’t like to talk. Maybe I just don’t like you.”An exceptional and well-rounded student, Chandar was one of the youngest accepted into the University of Cambridge. He majored in electronics and computer engineering, completing an internship at a prestigious tech company where he contributed to the design and function of security system prototypes. His ingenuity caught the attention of Britain’s national security office, the Government Communications Headquarters (GCHQ). Having demonstrated acuity in code breaking, Chandar became an intelligence officer within the signals intelligence (SIGINT) unit. He continues to contribute to the applied technical research team and has recently expanded his field of study to include learning Korean."
+      },
+      {
+        id:20,
+        operator: "Mute",
+        name: "Mark R. Chandar",
+        image: '',
+        gadget: "SIGNAL DISRUPTOR",
+        Bio:"“People think my codename is because I don’t like to talk. Maybe I just don’t like you.”An exceptional and well-rounded student, Chandar was one of the youngest accepted into the University of Cambridge. He majored in electronics and computer engineering, completing an internship at a prestigious tech company where he contributed to the design and function of security system prototypes. His ingenuity caught the attention of Britain’s national security office, the Government Communications Headquarters (GCHQ). Having demonstrated acuity in code breaking, Chandar became an intelligence officer within the signals intelligence (SIGINT) unit. He continues to contribute to the applied technical research team and has recently expanded his field of study to include learning Korean."
+      }
+    ]
+  ]
+]
+
+export default mock;
